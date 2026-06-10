fc45e1bd12c25d6393dcf488bf5843308dbd5ddb
Christofer Jesús Ulloa Rodríguez <christoferjur.1103@gmail.com>
2026-06-09 22:57:10 -0600

    Historia 11

---
+++ Proyecto1Cliente/Models/Domain/Fotografia.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Proyecto1Cliente.Models.Domain
+{
+    public class Fotografia
+    {
+        [JsonPropertyName("id_fotografia")]
+        public int IdFotografia { get; set; }
+
+        [JsonPropertyName("ruta")]
+        public string Ruta { get; set; }
+
+        [JsonPropertyName("id_especimen")]
+        public int IdEspecimen { get; set; }
+    }
+}
